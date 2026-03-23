c889a7f9aa5b8fbce8436d3a50bda1683def7466
Svetlana <yarullina6277@mail.ru>
2026-03-24 01:01:47 +0300

    add module 1 homework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { LogoComponent } from './core/logo/logo.component';
+import { CoursesComponent } from './features/courses/courses.component';
+import { CourseCardComponent } from './features/courses/components/course-card/course-card.component';
+import { CourseSearchComponent } from './features/courses/components/course-search/course-search.component';
+import { BreadcrumbsComponent } from './features/courses/components/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    CoursesComponent,
+    CourseCardComponent,
+    CourseSearchComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule,
