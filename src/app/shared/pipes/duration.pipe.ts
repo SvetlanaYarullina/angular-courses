8d3950b7b1733172c42dcba6addf0899e01eaeb1
Svetlana <yarullina6277@mail.ru>
2026-04-13 23:36:15 +0300

    add module 3 homework

--- src/app/shared/pipes/duration.pipe.ts
+++ src/app/shared/pipes/duration.pipe.ts
@@ -4,21 +4,18 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'duration'
 })
 export class DurationPipe implements PipeTransform {
-  
   transform(minutes: number): string {
-    if (!minutes && minutes !== 0) return '';
+    if (minutes < 60) {
+      return `${minutes} минут`;
+    }
     
     const hours = Math.floor(minutes / 60);
     const remainingMinutes = minutes % 60;
     
-    if (hours === 0) {
-      return `${remainingMinutes} min`;
-    }
-    
     if (remainingMinutes === 0) {
-      return `${hours} hour${hours > 1 ? 's' : ''}`;
+      return `${hours} час${hours > 1 ? 'а' : ''}`;
     }
     
-    return `${hours} hour${hours > 1 ? 's' : ''} ${remainingMinutes} min`;
+    return `${hours} час${hours > 1 ? 'а' : ''} ${remainingMinutes} минут`;
   }
 }
